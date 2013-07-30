683c63511173f3da84d772a74338edf225eda852
Ole Schütt <oschuett@cp2k.org>
2013-07-30 19:17:54 +0200

    added setup_s01.sh

---
+++ tools/setup_s01.sh
@@ -0,0 +1,8 @@
+set +x
+#source /opt/intel/bin/ifortvars.sh intel64
+source /data/vjoost/valgrind-3.8.0/setup
+#source /opt/intel/composerxe-2011.3.174/bin/ifortvars.sh intel64
+source /data/vjoost/gnu/gcc-4_7-branch/setup
+source /data/vjoost/oprofile-0.9.8/setup
+source /data/vjoost/openmpi-1.6.3/setup
+set -x
