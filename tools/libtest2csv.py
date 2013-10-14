786c0bd66029045a1ced7ef58c2dbf7ad3911416
Ole Schütt <oschuett@cp2k.org>
2013-10-14 13:20:55 +0200

    add tolerance

--- tools/libtest2csv.py
+++ tools/libtest2csv.py
@@ -8,17 +8,18 @@ import numpy as np
 
 #===============================================================================
 def main():
-    if(len(sys.argv) < 2):
-        print("libtest2csv.py <ref_checksum> <out-file1> ... <out-fileN>")
+    if(len(sys.argv) < 4):
+        print("libtest2csv.py <ref_checksum> <tolerance> <out-file1> ... <out-fileN>")
         sys.exit(1)
 
     table = dict()
     reference = float(sys.argv[1])
+	tolerance = float(sys.argv[2])
     files = sys.argv[2:]
     for fn in files:
         content = open(fn).read()
         checksum = float(re.search("Final checksums\s+(.+?)\s+", content).group(1))
-        assert(abs(checksum-reference) < 0.05)
+        assert(abs(checksum-reference) < tolerance)
         flops = np.array(re.findall("\s(\d+) Mflops/rank", content), np.int)
         flops_max = np.amax(flops[1:])
         key = fn.rsplit(".", 1)[0]
