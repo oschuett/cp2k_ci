31bb88320faeb02b17b5a9742ebbd3d535501ac0
Ole Schütt <oschuett@cp2k.org>
2013-11-01 13:40:45 +0100

    Changed order

--- tools/setup_s03.sh
+++ tools/setup_s03.sh
@@ -1,15 +1,15 @@
 set +x
-#source /opt/intel/bin/ifortvars.sh intel64
-source /data/vjoost/valgrind-3.8.0/setup
-source /opt/intel/composerxe-2011.3.174/bin/ifortvars.sh intel64
-source /data/vjoost/gnu/gcc-4_7-branch/setup
-source /data/vjoost/oprofile-0.9.8/setup
-source /data/vjoost/openmpi-1.6.3/setup
-
 # here the order matters :-(
 source /etc/profile.d/modules.sh
 module load intel-2013.0.079
 #module load openmpi-x86_64
 module load cuda-5.5.22-x86_64
+
+source /opt/intel/bin/ifortvars.sh intel64
+source /data/vjoost/valgrind-3.8.0/setup
+source /opt/intel/composerxe-2011.3.174/bin/ifortvars.sh intel64
+source /data/vjoost/gnu/gcc-4_7-branch/setup
+source /data/vjoost/oprofile-0.9.8/setup
+source /data/vjoost/openmpi-1.6.3/setup
 
 set -x
